947c8bfaf4792ea85ddf0470c8282fcc691064cb
bralexa <nice.bald.man@gmail.com>
2019-07-28 21:36:58 +0300

    28.07.19

---
+++ 28.07.19/main.js
@@ -0,0 +1,126 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Coordinates = /** @class */ (function () {
+    function Coordinates(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    return Coordinates;
+}());
+var Square = /** @class */ (function (_super) {
+    __extends(Square, _super);
+    function Square(x, y, color, length) {
+        var _this = _super.call(this, x, y) || this;
+        _this.color = color;
+        _this.length = length;
+        return _this;
+    }
+    Square.prototype.print = function () {
+        console.log("This is square, coordinates: (" + this.x + ", " + this.y + "), color: " + this.color + ", Side length: " + this.length + ", area is: " + this.AreaCalculation() + "sm^2, perimeter is: " + this.PerimeterCalculation() + "sm");
+        ;
+    };
+    Square.prototype.AreaCalculation = function () {
+        return Math.pow(this.length, 2);
+    };
+    Square.prototype.PerimeterCalculation = function () {
+        return 4 * this.length;
+    };
+    Square.prototype.draw = function () {
+        var _draw = '';
+        for (var i = 0; i < this.length; i++) {
+            for (var j = 0; j < this.length; j++) {
+                if (i == 0 || i == this.length - 1) {
+                    _draw += '*';
+                }
+                else if (j == 0 || j == this.length - 1) {
+                    _draw += '*';
+                }
+                else {
+                    _draw += ' ';
+                }
+            }
+            _draw += '\n';
+        }
+        return _draw;
+    };
+    return Square;
+}(Coordinates));
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle(x, y, color, radius) {
+        var _this = _super.call(this, x, y) || this;
+        _this.name = 'Circle';
+        _this.color = color;
+        _this.radius = radius;
+        return _this;
+    }
+    Circle.prototype.AreaCalculation = function () {
+        return 3.14 * (this.radius * this.radius);
+    };
+    Circle.prototype.PerimeterCalculation = function () {
+        return 2 * 3.14 * this.radius;
+    };
+    Circle.prototype.print = function () {
+        console.log("This is circle, coordinates: (" + this.x + ", " + this.y + "), color: " + this.color + ", Radius: " + this.radius + ", area is: " + this.AreaCalculation() + "sm^2, perimeter is: " + this.PerimeterCalculation() + "sm");
+        ;
+    };
+    return Circle;
+}(Coordinates));
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(x, y, color, length, height) {
+        var _this = _super.call(this, x, y) || this;
+        _this.name = 'Rectangle';
+        _this.color = color;
+        _this.length = length;
+        _this.height = height;
+        return _this;
+    }
+    Rectangle.prototype.AreaCalculation = function () {
+        return Math.pow(this.length, this.height);
+    };
+    Rectangle.prototype.PerimeterCalculation = function () {
+        return 2 * (this.length + this.height);
+    };
+    Rectangle.prototype.print = function () {
+        console.log("This is rectangle, coordinates: (" + this.x + ", " + this.y + "), color: " + this.color + ", length: " + this.length + ", Height: " + this.height + ", area is: " + this.AreaCalculation() + "sm^2, perimeter is: " + this.PerimeterCalculation() + "sm");
+    };
+    Rectangle.prototype.draw = function () {
+        var _draw = '';
+        for (var i = 0; i < this.length; i++) {
+            for (var j = 0; j < this.length; j++) {
+                if (i == 0 || i == this.length - 1) {
+                    _draw += '*';
+                }
+                else if (j == 0 || j == this.length - 1) {
+                    _draw += '*';
+                }
+                else {
+                    _draw += ' ';
+                }
+            }
+            _draw += '\n';
+        }
+        return _draw;
+    };
+    return Rectangle;
+}(Coordinates));
+var square = new Square(5, 5, 'Red', 4);
+square.print();
+console.log(square.draw());
+var circle = new Circle(3, 4, 'blue', 5);
+circle.print();
+var rectangle = new Rectangle(6, 7, 'Black', 6, 8);
+rectangle.print();
+console.log(rectangle.draw());
---
+++ 28.07.19/main.ts
@@ -0,0 +1,149 @@
+
+    interface IDrawable {
+        draw();
+        
+    }
+
+
+
+
+
+
+abstract class Coordinates {
+     x: number;
+     y: number;
+
+    constructor (x: number, y: number){
+
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+
+
+class Square extends Coordinates implements IDrawable{
+    private color: string;
+    private length: number;
+    
+
+    constructor (x:number, y:number, color:string, length: number){
+        super (x, y);
+        this.color = color;
+        this.length = length;
+        
+
+    }
+
+    print(){
+        console.log(`This is square, coordinates: (${this.x}, ${this.y}), color: ${this.color}, Side length: ${this.length}, area is: ${this.AreaCalculation()}sm^2, perimeter is: ${this.PerimeterCalculation()}sm`);
+        ;
+         
+    }
+
+    AreaCalculation(){
+        return Math.pow(this.length, 2);
+    }
+
+    PerimeterCalculation(){
+        return 4 * this.length;
+    }
+    draw() {
+        let _draw = '';
+        for (let i = 0; i < this.length; i++) {
+            for (let j = 0; j < this.length; j++) {
+                if (i == 0 || i == this.length - 1) {
+                    _draw += '*';
+                }
+                else if (j == 0 || j == this.length - 1) {
+                    _draw += '*';
+                } else {
+                    _draw += ' ';
+                }
+            }
+            _draw += '\n';
+        }
+        return _draw;
+    }
+
+}
+
+
+
+class Circle extends Coordinates{
+    private color: string;
+    private radius: number;
+    private name = 'Circle';
+    constructor (x:number, y:number, color:string, radius: number){
+        super (x, y);
+        this.color = color;
+        this.radius = radius;
+    }
+    AreaCalculation(){
+        return 3.14 * (this.radius * this.radius);
+    }
+    PerimeterCalculation(){
+        return 2 * 3.14 * this.radius;
+    }
+    print(){
+        console.log(`This is circle, coordinates: (${this.x}, ${this.y}), color: ${this.color}, Radius: ${this.radius}, area is: ${this.AreaCalculation()}sm^2, perimeter is: ${this.PerimeterCalculation()}sm`);
+        ;
+         
+    }
+
+}
+
+class Rectangle extends Coordinates implements IDrawable{
+    private color: string;
+    private length: number;
+    private height: number;
+    private name = 'Rectangle';
+
+    constructor (x:number, y:number, color:string, length: number, height:number){
+        super (x, y);
+        this.color = color;
+        this.length = length;
+        this.height = height;
+    }
+
+    AreaCalculation(){
+        return Math.pow(this.length, this.height);
+    }
+    PerimeterCalculation(){
+        return 2 * (this.length + this.height);
+    }
+    print(){
+        console.log(`This is rectangle, coordinates: (${this.x}, ${this.y}), color: ${this.color}, length: ${this.length}, Height: ${this.height}, area is: ${this.AreaCalculation()}sm^2, perimeter is: ${this.PerimeterCalculation()}sm`);
+    
+    }
+    draw() {
+        let _draw = '';
+        for (let i = 0; i < this.length; i++) {
+            for (let j = 0; j < this.length; j++) {
+                if (i == 0 || i == this.length - 1) {
+                    _draw += '*';
+                }
+                else if (j == 0 || j == this.length - 1) {
+                    _draw += '*';
+                } else {
+                    _draw += ' ';
+                }
+            }
+            _draw += '\n';
+        }
+        return _draw;
+    }
+}
+
+var square = new Square(5,5,'Red',4);
+square.print();
+console.log(square.draw());
+
+var circle = new Circle(3,4,'blue',5);
+circle.print();
+var rectangle = new Rectangle(6,7,'Black',6,8);
+rectangle.print();
+console.log(rectangle.draw());
+
+
