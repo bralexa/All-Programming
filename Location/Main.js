2affe5e216d086461867eac77140e34f84be4c3b
bralexa <nice.bald.man@gmail.com>
2019-08-10 13:22:00 +0300

    up

--- Location/Main.js
+++ Location/Main.js
@@ -1,27 +1,20 @@
-if(localStorage.getItem('0')){
-    return;
-}else {
-    document.getElementById("form").innerHTML="  "
+if (localStorage.getItem('0')) {
+    document.getElementById("form").innerHTML = " ";
+} else {
+    document.getElementById("submit").addEventListener("click", setTolocalStorage);
+    function setTolocalStorage() {
+        named = namer.value;
+        console.log(named);
+        if (document.getElementById("checkbox").checked) {
+            localStorage.setItem('0', named);
+            sessionStorage.setItem('0', named);
+        } else {
+            sessionStorage.setItem('0', named);
+        }
+    }
 }
 
-document.getElementById("submit").addEventListener("click", setTolocalStorage);
-function setTolocalStorage() {
-    named = namer.value;
-    console.log(named);
-    if(document.getElementById("checkbox").checked){
-        if(localStorage.getItem('0')){
-            return;
-        }else{
-            localStorage.setItem('0',named);
-        }
-        
-    }else{
-        sessionStorage.setItem('0',named)
-    }
-    
-    
-    
-}
+
 
 
 
