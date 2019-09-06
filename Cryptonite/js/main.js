63948a63d0c759ea9c1a8cc6408dc4c5f1d811a9
bralexa <nice.bald.man@gmail.com>
2019-09-06 18:23:03 +0300

    up

--- Cryptonite/js/main.js
+++ Cryptonite/js/main.js
@@ -14,7 +14,7 @@ function getCoins() {
         url: all_coins_url,
         async: false,
         success: function (data) {
-            coins = data.splice(100, 100);
+            coins = data.splice(2000, 100);
             coins.forEach((value) => { coinsArray.push([value.id, value.name, value.symbol]) });
             window.localStorage.coinsList = JSON.stringify(coins);
 
@@ -24,7 +24,7 @@ function getCoins() {
                 var percent = '<div class="container" id="overallProgress"><h3>Loading coins...' + counter + '%</h3><div class="box progress" style="height: 2rem;"><div class="progress-bar progress-bar-striped progress-bar-animated" style="width:' + counter + '%; height: 100%;"></div></div></div>';
                 $('.main-info').html(percent);
                 counter++;
-                if (counter === 101) {
+                if (counter === 100) {
                     clearInterval(i);
                     $('#overallProgress').empty();
                     for (let i = 0; i < 100; i++) {
@@ -33,6 +33,7 @@ function getCoins() {
                         var name = element[1];
                         var symbol = element[2];
                         createCard(symbol, name, id);
+
                     }
                 }
             }, 20);
@@ -46,34 +47,34 @@ function getCoins() {
     })
 }
 
-function moreInfo(symbol) {
-    var singe_url = single_coin_url + symbol;
+function moreInfo(getId) {
+    var singe_url = single_coin_url + getId;
 
     var counter = 0;
     var i = setInterval(function () {
         var percent = '<div class="container"><div class="progress"><div class="progress-bar progress-bar-striped progress - bar - animated" style="width:' + counter + '%;">' + counter + '%</div></div></div>';
-        $('#inforow_' + symbol).html(percent);
+        $('#inforow_' + getId).html(percent);
         counter++;
-        if (counter === 101) {
+        if (counter === 99) {
             clearInterval(i);
             $.ajax({
                 type: 'GET',
                 datatype: 'json',
                 url: singe_url,
                 async: false,
-                id: symbol,
+                id: getId,
                 success: function (data) {
                     var image_url = data.image.large;
                     var usd_price = data.market_data.current_price.usd;
                     var eur_price = data.market_data.current_price.eur;
                     var ils_price = data.market_data.current_price.ils;
                     string = '<div class="container col"><img src = "' + image_url + '" alt = "" class="logo-img" ></div><div class="container col moreinfo"><p>' + eur_price + '<strong> €</strong></p><p>' + usd_price + '<strong> $</strong></p><p>' + ils_price + '<strong> ₪</strong></p></div>'
-                    $('#inforow_' + symbol).html(string);
-                    $('#' + symbol).text('Close');
-                    $('#' + symbol).attr({
+                    $('#inforow_' + getId).html(string);
+                    $('#' + getId).text('Close');
+                    $('#' + getId).attr({
                         onclick: 'clearInfo(this.id)'
-
                     });
+                    setSwitchOff(getId);
                 },
                 error: function (error) {
                     console.log("error : ", error)
@@ -83,7 +84,7 @@ function moreInfo(symbol) {
                 }
             })
         }
-    }, 5);
+    }, 10);
 }
 
 function clearInfo(id) {
@@ -98,21 +99,27 @@ function createCard(symbol, name, id) {
     var string = '<div class="container col-sm-4 box-shaded" id="container_' + symbol + '">';
     string += '<div class="row justify-content-between"><div class="container col"><h2>' + symbol + '</h2>';
     string += '</div><div class="custom-control custom-switch"><div class="checkbox checkbox-slider--b-flat checkbox-slider-md">';
-    string += '<label><input type="checkbox" onchange="handleChange(this, this.id)" id="' + symbol + '"><span></span></label></div></div></div>';
+    string += '<label><input type="checkbox"  onchange="handleSwitch(this, this.name)" name="' + id + '" id="checkbox_' + id + '"><span></span></label></div></div></div>';
     string += '<div><p class="fullname">' + name + '</p></div><div class="row info" id="inforow_' + id + '"></div>';
     string += '<button type="button" class="btn btn-outline-secondary" onclick="moreInfo(this.id)" id="' + id + '">More info</button></div>';
     $('.for_insert').append(string);
 
 }
 
-function handleChange(checkbox, checkedCoin) {
+function handleSwitch(checkbox, checkedCoin) {
 
     var checkedId = checkedCoin;
 
 
     if (checkbox.checked == true) {
-        alert(checkedId);
+
+
     } else {
         alert(checkedId);
     }
+}
+
+function setSwitchOff(id) {
+    var checkboxFinder = $('#checkbox_' + id).find('.switch').find('input');
+    checkboxFinder.prop('checked', true);
 }
