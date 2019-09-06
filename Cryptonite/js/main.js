89a383e1d3e2328acfc52bc9fb34be8df281ab02
bralexa <nice.bald.man@gmail.com>
2019-09-06 18:56:03 +0300

    up

--- Cryptonite/js/main.js
+++ Cryptonite/js/main.js
@@ -112,7 +112,7 @@ function handleSwitch(checkbox, checkedCoin) {
 
 
     if (checkbox.checked == true) {
-
+        modal.style.display = "block";
 
     } else {
         alert(checkedId);
@@ -122,4 +122,31 @@ function handleSwitch(checkbox, checkedCoin) {
 function setSwitchOff(id) {
     var checkboxFinder = $('#checkbox_' + id).find('.switch').find('input');
     checkboxFinder.prop('checked', true);
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
