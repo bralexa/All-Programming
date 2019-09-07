db4cbc8770ec96efd23ef6191f92a2e54f6da746
bralexa <nice.bald.man@gmail.com>
2019-09-07 22:52:43 +0300

    up

--- Cryptonite/js/main.js
+++ Cryptonite/js/main.js
@@ -30,7 +30,7 @@ function getCoins() {
             checkLocalstorage();
             var modal = document.getElementById("myModal");
             modal.style.display = "block";
-            
+
             var counter = 0;
             var i = setInterval(function () {
                 // do your thing
@@ -184,19 +184,42 @@ function checkLocalstorage() {
     }
 }
 
+function liveReports() {
+    if (localStorage.length == 0) {
+        var modal = document.getElementById("myModal");
+        var span = document.getElementsByClassName("close")[0];
+        modal.style.display = "block";
+        
+        var message = '<span class="close text-right">&times;</span><div class="container"><h2 class="text-center">No coins choosen!</h2></div > ';
+        $('.modal-content').html(message);
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+        
+
+    }
+
+}
+
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+// btn.onclick = function () {
+//     modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
