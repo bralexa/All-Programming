0ddf3b32ebdcbba1b5d4bcbf4a54f102cc40c19e
bralexa <nice.bald.man@gmail.com>
2019-09-08 13:47:05 +0300

    up

--- Cryptonite/js/main.js
+++ Cryptonite/js/main.js
@@ -2,7 +2,8 @@ const all_coins_url = 'https://api.coingecko.com/api/v3/coins/list';
 const single_coin_url = 'https://api.coingecko.com/api/v3/coins/'
 var coinsArray = [];
 getCoins();
-
+// Get the modal
+var modal = document.getElementById("myModal");
 
 
 
@@ -133,7 +134,7 @@ function createMiniCards() {
 
         }
 
-        
+
         for (let i = 0; i < values.length; i++) {
             for (let j = 0; j < coinsArray.length; j++) {
                 var element = coinsArray[j];
@@ -284,9 +285,6 @@ function liveReports() {
             }
 
         }
-
-
-
     }
 
 }
@@ -296,14 +294,13 @@ function closeModal() {
     modal.style.display = "none";
 
 }
-// Get the modal
-var modal = document.getElementById("myModal");
+
 
 // Get the button that opens the modal
 // var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 // btn.onclick = function () {
@@ -311,13 +308,13 @@ var span = document.getElementsByClassName("close")[0];
 // }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
+// span.onclick = function () {
+//     modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
