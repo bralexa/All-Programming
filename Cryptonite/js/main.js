1d3b5a8045c557ea85df29703182254a9e4111a6
bralexa <nice.bald.man@gmail.com>
2019-09-11 20:29:03 +0300

    up

--- Cryptonite/js/main.js
+++ Cryptonite/js/main.js
@@ -14,46 +14,62 @@ checkLocalstorage();
 function getCoins() {
     $('.for_insert').empty();
 
+    var counter = 0;
+    var i = setInterval(function () {
+
+        var percent = '<div class="card box-shaded" id="overallProgress"><h4 class="text-center">Loading coins...   ' + counter + '%</h4><div class="box progress" style="height: 2rem;"><div class="progress-bar progress-bar-animated" style="width:' + counter + '%; height: 100%;"></div></div></div>';
+        $('.forprogress-bar').html(percent);
+        counter++;
+        if (counter === 100) {
+            clearInterval(i);
+            var message = '<div class="card box-shaded" id="overallProgress"><h4 class="text-center">Still waiting...  </h4></div>';
+            $('.forprogress-bar').html(message);
+        }
+    }, 10);
     $.ajax({
         type: 'GET',
         datatype: 'json',
         url: all_coins_url,
-        async: false,
+        async: true,
         success: function (data) {
             data.forEach((value) => { coinsArray.push([value.id, value.name, value.symbol]) });
 
 
             coins = data.splice(500, 100);
             coins.forEach((value) => { viewArray.push([value.id, value.name, value.symbol]) });
+            //var modal = document.getElementById("myModal");
+            //modal.style.display = "block";
+
+            
+
+
+
+        },
+        error: function (error) {
             var modal = document.getElementById("myModal");
             modal.style.display = "block";
 
-            var counter = 0;
-            var i = setInterval(function () {
-                // do your thing
-                var percent = '<div class="container" id="overallProgress"><h3 class="text-center">Loading coins...' + counter + '%</h3><div class="box progress" style="height: 2rem;"><div class="progress-bar progress-bar-striped progress-bar-animated" style="width:' + counter + '%; height: 100%;"></div></div></div>';
-                $('.modal-content').html(percent);
-                counter++;
-                if (counter === 100) {
-                    clearInterval(i);
-                    $('#overallProgress').empty();
+            var message = '<div class="container"><p class="text-center">No connection!</p></div > ';
+            $('.modal-content').html(message);
+            window.onclick = function (event) {
+                if (event.target == modal) {
                     modal.style.display = "none";
+                    closeModal();
                 }
-            }, 20);
+
+            };
+        },
+        complete: function () {
             for (let i = 0; i < 100; i++) {
                 var element = viewArray[i];
                 var id = element[0];
                 var name = element[1];
                 var symbol = element[2];
                 createCard(symbol, name, id);
-
+                
             }
-        },
-        error: function (error) {
-            console.log("error : ", error);
-        },
-        complete: function () {
-
+            clearInterval(i);
+            $('.forprogress-bar').empty();
         }
     });
 
@@ -65,37 +81,53 @@ function moreInfo(getId) {
     var counter = 0;
     var i = setInterval(function () {
         var percent = '<div class="container"><div class="progress"><div class="progress-bar progress-bar-striped progress - bar - animated" style="width:' + counter + '%;">' + counter + '%</div></div></div>';
-        $('#inforow_' + getId).html(percent);
+        $('#progressrow_' + getId).html(percent);
         counter++;
         if (counter === 100) {
             clearInterval(i);
-            $.ajax({
-                type: 'GET',
-                datatype: 'json',
-                url: singe_url,
-                async: false,
-                success: function (data) {
-                    var image_url = data.image.large;
-                    var usd_price = data.market_data.current_price.usd;
-                    var eur_price = data.market_data.current_price.eur;
-                    var ils_price = data.market_data.current_price.ils;
-                    string = '<div class="container box-shaded col d-flex align-items-center justify-content-between><div class="container col"><img src = "' + image_url + '" class="logo-img" ></div><div class="container box-shaded col moreinfo text-center"><p><strong>Price:</strong></p><h5>' + eur_price + '<strong> €</strong></h5><h5>' + usd_price + '<strong> $</strong></h5><h5>' + ils_price + '<strong> ₪</strong></h5></div></div>'
-                    $('#inforow_' + getId).html(string);
-                    $('#' + getId).text('Close info');
-                    $('#' + getId).attr({
-                        onclick: 'clearInfo(this.id)'
-                    });
-
-                },
-                error: function (error) {
-                    console.log("error : ", error);
-                },
-                complete: function () {
-
-                }
-            });
+            var message = '<div class="container"><div class="progress"><div class="progress-bar progress-bar-striped progress - bar - animated" style="width:' + counter + '%;">Still waiting...</div></div></div>';
+            $('#progressrow_' + getId).html(message);
+            
         }
     }, 10);
+    $.ajax({
+        type: 'GET',
+        datatype: 'json',
+        url: singe_url,
+        async: true,
+        success: function (data) {
+            var image_url = data.image.large;
+            var usd_price = data.market_data.current_price.usd;
+            var eur_price = data.market_data.current_price.eur;
+            var ils_price = data.market_data.current_price.ils;
+            string = '<div class="container box-shaded col d-flex align-items-center justify-content-between><div class="container col"><img src = "' + image_url + '" class="logo-img" ></div><div class="container box-shaded col moreinfo text-center"><p><strong>Price:</strong></p><h5>' + eur_price + '<strong> €</strong></h5><h5>' + usd_price + '<strong> $</strong></h5><h5>' + ils_price + '<strong> ₪</strong></h5></div></div>'
+
+
+        },
+        error: function () {
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block";
+
+            var message = '<div class="container"><p class="text-center">No connection!</p></div > ';
+            $('.modal-content').html(message);
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                    closeModal();
+                }
+
+            };
+        },
+        complete: function () {
+            $('#inforow_' + getId).html(string);
+            $('#' + getId).text('Close info');
+            $('#' + getId).attr({
+                onclick: 'clearInfo(this.id)'
+            });
+            clearInterval(i);
+            $('#progressrow_' + getId).empty();
+        }
+    });
 }
 
 function clearInfo(id) {
@@ -111,7 +143,7 @@ function createCard(symbol, name, id) {
     string += '<div class="row justify-content-between name="' + id + '"><div class="container col"><h2>' + symbol + '</h2>';
     string += '</div><div class="custom-control custom-switch"><div class="checkbox checkbox-slider--b-flat checkbox-slider-md">';
     string += '<label><input type="checkbox"  onchange="handleSwitch(this, this.name)" name="' + id + '" id="checkbox_' + id + '"><span></span></label></div></div></div>';
-    string += '<div><p class="fullname">' + name + '</p></div><div class="row info" id="inforow_' + id + '"></div>';
+    string += '<div><p class="fullname">' + name + '</p></div><div class="row info" id="progressrow_' + id + '"></div><div class="row info" id="inforow_' + id + '"></div>';
     string += '<div class="container text-center"><button type="button" class="btn btn-outline-secondary box-shaded" onclick="moreInfo(this.id)" id="' + id + '">More info</button></div></div>';
     $('.for_insert').append(string);
 
@@ -125,6 +157,7 @@ function createSearchCard(symbol, name, id) {
     string += '<label><input type="checkbox"  onchange="handleSwitch(this, this.name)" name="' + id + '" id="checkbox_' + id + '"><span></span></label></div></div></div>';
     string += '<div><p class="fullname">' + name + '</p></div><div class="row info" id="inforow_' + id + '"></div>';
     string += '<div class="container text-center"><button type="button" class="btn btn-outline-secondary box-shaded" onclick="moreInfo(this.id)" id="' + id + '">More info</button></div></div>';
+    $('.modal-content').empty();
     $('.for_insert').append(string);
 
 }
@@ -133,9 +166,14 @@ function createSearchCard(symbol, name, id) {
 function createMiniCards() {
     var modal = document.getElementById("myModal");
     modal.style.display = "block";
+
     var header = '<h4 class="text-center"><strong>Live reports can be produced for&nbsp;5&nbsp;coins&nbsp;only.</br>Please review your choices.</strong></h4>';
     $('.modal-content').append(header);
     if (localStorage.length > 0) {
+
+
+
+
         var values = [],
             keys = Object.keys(localStorage),
             i = keys.length;
@@ -144,6 +182,7 @@ function createMiniCards() {
             values.push(localStorage.getItem(keys[i]));
 
         }
+
 
         for (let i = 0; i < values.length; i++) {
             for (let j = 0; j < coinsArray.length; j++) {
@@ -175,7 +214,7 @@ function createMiniCards() {
 
         }
 
-    };
+    }
 
 }
 
@@ -192,12 +231,12 @@ function aboutMe() {
 
 
     var string = '<div class="container col box-shaded"><h3 class="text-center"><strong>Alexander Bruder</strong></h3>';
-    string += '<div class="container col text-center"><img class="box-shaded img-fluid" src="img/Me.jpg"></div><div class="container text-center"><h3>Full Stack Developer</h3></div>';
-    string += '<div class="container text-center"><p><strong>Contacts:</strong></p></div><div class="container text-center"><p>Mobile: <a href="tel:+972548887511">+972548887511</a></p></div><div class="container text-center"><p>E-mail: <a href="mailto:nive.bald.man@gmail.com">nice.bald.man@gmail.com</a></p></div>';
+    string += '<div class="container col-sm-4 text-center"><img class="box-shaded img-fluid" src="img/Me.jpg"></div><div class="container text-center"><h4>Full-Stack Dev</h4></div>';
+    string += '<div class="container text-center"><p><strong>Contacts:</strong></p></div><div class="container text-center"><p>Mobile: <a href="tel:+972548887511">+972548887511</a></p><p>E-mail: <a class="text-center" href="mailto:nive.bald.man@gmail.com">nice.bald.man@gmail.com</a></p></div>';
     $('.modal-content').empty();
     $('.modal-content').append(string);
 
-    var button = '<div class="container text-center"><button type="button" class="btn btn-outline-secondary box-shaded" onClick="closeModal()">Close</button></div>';
+    var button = '<div class="container text-center"><button type="button" class="btn btn-outline-secondary box-shaded" onClick="closeModal()">Back</button></div>';
 
     $('.modal-content').append(button);
     window.onclick = function (event) {
@@ -303,7 +342,7 @@ function liveReports() {
                 closeModal();
             }
 
-        };
+        }
     } else {
 
         chartCoins();
@@ -328,7 +367,7 @@ function coinSearcher() {
         var span = document.getElementsByClassName("close");
         modal.style.display = "block";
 
-        var message = '<span class="close text-right" onClick="closeModal()">&times;</span><div class="container"><p class="text-center">Please enter some keywords for search!</p></div > ';
+        var message = '<span class="close text-right" onClick="closeModal()">&times;</span><div class="container"><p class="text-center">Please enter id, short symbol or name of coin!</p></div > ';
         $('.modal-content').html(message);
         window.onclick = function (event) {
             if (event.target == modal) {
@@ -336,7 +375,7 @@ function coinSearcher() {
                 closeModal();
             }
 
-        };
+        }
         return;
     } else {
         for (let j = 0; j < coinsArray.length; j++) {
@@ -350,6 +389,8 @@ function coinSearcher() {
                 checkLocalstorage();
                 counter++;
 
+            } else {
+
             }
         }
     }
@@ -358,7 +399,7 @@ function coinSearcher() {
         var span = document.getElementsByClassName("close");
         modal.style.display = "block";
 
-        var message = '<span class="close text-right" onClick="closeModal()">&times;</span><div class="container"><p class="text-center">Sorry, your search is no results!</p></div > ';
+        var message = '<span class="close text-right" onClick="closeModal()">&times;</span><div class="container"><p class="text-center">Sorry, your search returned no results!</p></div > ';
         $('.modal-content').html(message);
         window.onclick = function (event) {
             if (event.target == modal) {
@@ -366,11 +407,10 @@ function coinSearcher() {
                 closeModal();
             }
 
-        };
+        }
     }
 }
 
-//chart
 
 function chartCoins() {
     var coinsForLive = [];
