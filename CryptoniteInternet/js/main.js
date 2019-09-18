ac58a44953619ec6eb12f4054f4fdd4a65d5772a
bralexa <nice.bald.man@gmail.com>
2019-09-18 20:46:44 +0300

    up

--- CryptoniteInternet/js/main.js
+++ CryptoniteInternet/js/main.js
@@ -264,9 +264,9 @@ function closeModal() {//סוגר חלון מודל
 
 }
 function aboutMe() {//מייצר כרטיסיה עם מידה עבורי
-    var string = '<div class="container col-sm-6 align-items-center box-shaded"><div class="container"><h3 class="text-center"><strong>Alexander Bruder</strong></h3>';
+    var string = '<div class="container col-sm-6 align-items-center shadow p-3 mb-5 bg-white rounded"><div class="container"><h3 class="text-center"><strong>Alexander Bruder</strong></h3>';
     string += '<div class="container col-sm-6 text-center"><img class="box-shaded img-fluid" src="img/Me.jpg"></div><h5 class="text-center">Full-Stack Dev</h5>';
-    string += '<div class="card-header box-shaded"><h6 class="text-center"><strong>Contact me:</strong></h6><p class="card text-center">Mobile:</br><a href="tel:+972548887511">+972548887511</a></p><p class="card text-center">E-mail:</br><a class="text-center" href="mailto:nive.bald.man@gmail.com">nice.bald.man@gmail.com</a></p></div></div></div>';
+    string += '<div class="card-header shadow p-3 mb-5 bg-white rounded"><h6 class="text-center"><strong>Contact me:</strong></h6><p class="card text-center">Mobile:</br><a href="tel:+972548887511">+972548887511</a></p><p class="card text-center">E-mail:</br><a class="text-center" href="mailto:nive.bald.man@gmail.com">nice.bald.man@gmail.com</a></p></div></div></div>';
     $('.for_insert').empty();
     $('.for_insert').append(string);
     $('html, body, .parallax').animate({
